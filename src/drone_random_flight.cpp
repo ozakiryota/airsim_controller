754a731c9f94729bcfbda0ec90883d97bc303e97
ozakiryota <kb22056@gmail.com>
2020-07-06 12:37:53 +0900

    updated

--- src/drone_random_flight.cpp
+++ src/drone_random_flight.cpp
@@ -6,29 +6,37 @@ int main()
 	// using namespace std;
 	msr::airlib::MultirotorRpcLibClient client;
 
-	std::cout << "Press Enter to enable API control" << std::endl;
-	std::cin.get();
+	std::cout << "Enable API control" << std::endl;
 	client.enableApiControl(true);
 
-	std::cout << "Press Enter to arm the drone" << std::endl;
-	std::cin.get();
+	std::cout << "Arm the drone" << std::endl;
 	client.armDisarm(true);
 
-	std::cout << "Press Enter to takeoff" << std::endl;
-	std::cin.get();
-	client.takeoffAsync(5)->waitOnLastTask();
+	std::cout << "Take off" << std::endl;
+	client.takeoffAsync(5.0)->waitOnLastTask();
 
-	std::cout << "Press Enter to rotation" << std::endl;
-	std::cin.get();
-	client.moveByRollPitchYawZAsync(M_PI/4.0, 0.0, 0.0, 0.0, 5.0)->waitOnLastTask();
+	MultirotorState position = client.getMultirotorState().getPosition();
 
-	std::cout << "Press Enter to move 5 meters in x direction with 1 m/s velocity" << std::endl;
-	std::cin.get();  
-	auto position = client.getMultirotorState().getPosition(); // from current location
-	client.moveToPositionAsync(position.x() + 5, position.y(), position.z(), 1)->waitOnLastTask();
+	std::cout << "Rotation" << std::endl;
+	client.moveByRollPitchYawZAsync(M_PI/4.0, 0.0, 0.0, position.z, 5.0)->waitOnLastTask();
 
-	std::cout << "Press Enter to land" << std::endl;
-	std::cin.get();
+	const int num_samle = 100;
+	for(int i=0; i<num_samle; ++i)
+		position = client.getMultirotorState().getPosition();
+		double x = position.x() + 1.0;
+		double y = position.y() + 1.0;
+		double z = position.z() + 1.0;
+		double vel = 5.0;
+		client.moveToPositionAsync(x, y, y, vel)->waitOnLastTask();
+		std::cout << "Move to ("
+			<< x << ", "
+			<< y << ", "
+			<< z << ") with"
+			<< vel << "[m/s] "
+			<< std::endl;
+	}
+
+	std::cout << "Land" << std::endl;
 	client.landAsync()->waitOnLastTask();
 
 	return 0;
