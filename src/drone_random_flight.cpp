e3ec493c495218e48f57fd83f080f903c5d9c2fe
ozakiryota <kb22056@gmail.com>
2020-07-06 17:15:11 +0900

    updated

--- src/drone_random_flight.cpp
+++ src/drone_random_flight.cpp
@@ -4,6 +4,7 @@
 int main() 
 {
 	msr::airlib::MultirotorRpcLibClient client;
+	client.reset();
 
 	std::cout << "Enable API control" << std::endl;
 	client.enableApiControl(true);
@@ -17,17 +18,23 @@ int main()
 	Eigen::Matrix<float, 3, 1> position = client.getMultirotorState().getPosition();
 
 	std::cout << "Rotation" << std::endl;
-	client.moveByRollPitchYawZAsync(M_PI/4.0, 0.0, 0.0, position.z(), 1.0)->waitOnLastTask();
+	client.moveByRollPitchYawZAsync(M_PI/6.0, 0.0, 0.0, position.z(), 1.0)->waitOnLastTask();
 
+	client.simPause(true);
+	std::cout << "Press enter to start sampling" << std::endl;
 	std::cin.get();
-	const int num_samle = 100;
+	client.simPause(false);
+	const int num_samle = 10;
 	for(int i=0; i<num_samle; ++i){
+		std::cout << "samle " << i << std::endl;
 		position = client.getMultirotorState().getPosition();
+		msr::airlib::CollisionInfo collision_info = client.simGetCollisionInfo();
+		std::cout << "collision_info.has_collided = " << (bool)collision_info.has_collided << std::endl;
+
 		double x = position.x() + 1.0;
 		double y = position.y() + 1.0;
-		double z = position.z() + 1.0;
-		double vel = 5.0;
-		std::cout << "samle " << i << " start" << std::endl;
+		double z = position.z() - 1.0;
+		double vel = 1.0;
 		client.moveToPositionAsync(x, y, y, vel)->waitOnLastTask();
 		std::cout << "Move to ("
 			<< x << ", "
@@ -35,7 +42,6 @@ int main()
 			<< z << ") with "
 			<< vel << "[m/s] "
 			<< std::endl;
-		std::cout << "samle " << i << " end" << std::endl;
 	}
 
 	std::cout << "Land" << std::endl;
