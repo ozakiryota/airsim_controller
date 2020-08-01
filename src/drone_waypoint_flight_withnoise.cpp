02b9ca91031e876299ae5e273bd089266a43d18e
ozakiryota <kb22056@gmail.com>
2020-08-01 23:00:54 +0900

    updated

---
+++ src/drone_waypoint_flight_withnoise.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include "vehicles/multirotor/api/MultirotorRpcLibClient.hpp"
+
+class DroneWayPointFlight{
+	private:
+		msr::airlib::MultirotorRpcLibClient _client;
+		std::vector<Eigen::Vector3f> _path;
+
+	public:
+		DroneWayPointFlight();
+		void setWayPoint(void);
+		void clientInitialization(void);
+		void startFlight(void);
+};
+
+DroneWayPointFlight::DroneWayPointFlight()
+{
+	std::cout << "----- drone_waypoint_flight -----" << std::endl;
+	/*initialize*/
+	setWayPoint();
+	clientInitialization();
+}
+
+void DroneWayPointFlight::setWayPoint(void)
+{
+	const double height = height;
+	_path = {
+		Eigen::Vector3f(128.0, 0.0, height),
+		Eigen::Vector3f(128.0, 128.0, height),
+		Eigen::Vector3f(0.0, 128.0, height),
+		Eigen::Vector3f(-128.0, 128.0, height),
+		Eigen::Vector3f(-128.0, 0.0, height),
+		Eigen::Vector3f(-128.0, -128.0, height),
+		Eigen::Vector3f(0.0, -128.0, height),
+		Eigen::Vector3f(128.0, -128.0, height),
+		Eigen::Vector3f(0.0, -128.0, height),
+		Eigen::Vector3f(0.0, 0.0, height)
+	};
+}
+
+void DroneWayPointFlight::clientInitialization(void)
+{
+	/*connect*/
+	_client.confirmConnection();
+	/*reset*/
+	std::cout << "Reset" << std::endl;
+	_client.reset();
+	/*control*/
+	std::cout << "Enable API control" << std::endl;
+	_client.enableApiControl(true);
+	std::cout << "Arm the drone" << std::endl;
+	_client.armDisarm(true);
+	std::cout << "Take off" << std::endl;
+	_client.takeoffAsync()->waitOnLastTask();
+}
+
+void DroneWayPointFlight::startFlight(void)
+{
+	std::cout << "startFlight" << std::endl;
+	const double vel = 5.0;
+	YawMode yaw_mode;
+	yaw_mode.is_rate = false;
+	yaw_mode.yaw_or_rate = 0;
+	_client.moveOnPathAsync(
+		_path,
+		vel,
+		Utils::max<float>(),
+		DrivetrainType::ForwardOnly,
+		yaw_mode
+	)->waitOnLastTask();
+}
+
+int main(void) 
+{
+	DroneWayPointFlight drone_waypoint_flight;
+	drone_waypoint_flight.startFlight();
+
+	return 0;
+}
